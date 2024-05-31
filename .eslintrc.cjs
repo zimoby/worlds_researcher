23fc914f68e6538d030d506ee2b70c35176c827e
Denys Bondartsov <zimoby@gmail.com>
2024-06-01 01:14:52 +0300

    update packages and configs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,14 +6,21 @@ module.exports = {
     browser: true,
     es2020: true,
     node: true },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended-type-checked',
-    `plugin:@typescript-eslint/stylistic-type-checked`,
-    'plugin:react/recommended',
+    "plugin:react/recommended",
     `plugin:react/jsx-runtime`,
     'plugin:react-hooks/recommended',
-    'plugin:prettier/recommended',
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    `plugin:@typescript-eslint/stylistic-type-checked`,
+    "plugin:prettier/recommended",
+    "plugin:tailwindcss/recommended",
     'prettier'
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs', 'node_modules'],
@@ -26,26 +33,29 @@ module.exports = {
       jsx: true
     }
   },
-  plugins: ['react-refresh', '@typescript-eslint', 'react', 'react-hooks', 'prettier'],
+  plugins: ['react-refresh', '@typescript-eslint', 'react', 'react-hooks', 'prettier', 'tailwindcss'],
   rules: {
-    'prettier/prettier': 'error',
-    '@typescript-eslint/no-unused-vars': 'error',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    'react/prop-types': 'off',
-    'react/react-in-jsx-scope': 'off',
-    'react/jsx-uses-react': 'off',
-    'react/jsx-uses-vars': 'error',
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
+
+    "react/prop-types": "off",
+    "react/react-in-jsx-scope": "off",
+    'react/jsx-uses-react': 'off',
+    'react/jsx-uses-vars': 'error',
     'react/no-unknown-property': 'off',
+    
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/no-non-null-assertion": "off",
+    '@typescript-eslint/no-unused-vars': 'error',
+
+    "tailwindcss/classnames-order": "warn",
+    "tailwindcss/no-custom-classname": "warn",
+    "tailwindcss/no-contradicting-classname": "error",
+
+    'prettier/prettier': 'error',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true }
     ]
-  },
-  settings: {
-    react: {
-      version: 'detect'
-    }
   }
 }
